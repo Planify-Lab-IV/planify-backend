690890b42b7ffe7b14e180ce97a1eb981ddae14c
LucioZampella <lzluciozampella@gmail.com>
2026-08-27 21:22:37 -0300

    PLANIFY-135: Prisma version updated and prisma.ts URL one way call

--- src/infrastructure/prisma.ts
+++ src/infrastructure/prisma.ts
@@ -3,5 +3,5 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL! });
 export const prisma = new PrismaClient({ adapter });
