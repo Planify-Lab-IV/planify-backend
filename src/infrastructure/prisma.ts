6206bac770e33154310db55fbb4171cb04828def
LucioZampella <lzluciozampella@gmail.com>
2026-08-28 11:06:39 -0300

    PLANIFY-135: DATABASE_URL correctly import at prisma.ts

--- src/infrastructure/prisma.ts
+++ src/infrastructure/prisma.ts
@@ -2,6 +2,7 @@
 
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "../shared/config/env.js";
 
 const adapter = new PrismaPg({ connectionString: env.DATABASE_URL! });
 export const prisma = new PrismaClient({ adapter });
